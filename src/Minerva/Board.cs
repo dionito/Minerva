edb24bf5c0ad3dcd6240c391106228d5a5db8d25
dionito <12321827+dionito@users.noreply.github.com>
2024-05-29 18:14:07 -0700

    Code for issues #3, #4, #5

--- src/Minerva/Board.cs
+++ src/Minerva/Board.cs
@@ -6,7 +6,8 @@
 /// A bitboard is a 64-bit integer where each bit represents a square on the board. In our
 /// case, a bit set to 1 means that a piece is present on that square.
 ///
-/// H1 is represented by the most significant bit (MSB) and A8 is represented by the least (LSB).
+/// A8 is represented by the most significant bit (MSB) and
+/// H1 is represented by the least significant one (LSB)
 /// 
 /// 8 ▓ ▒ ▓ ▒ ▓ ▒ ▓ ▒ 
 /// 7 ▒ ▓ ▒ ▓ ▒ ▓ ▒ ▓ 
@@ -21,23 +22,23 @@
 /// </summary>
 public class Board
 {
-    public const ulong FileA = 0x0101010101010101ul;
-    public const ulong FileB = 0x0202020202020202ul;
-    public const ulong FileC = 0x0404040404040404ul;
-    public const ulong FileD = 0x0808080808080808ul;
-    public const ulong FileE = 0x1010101010101010ul;
-    public const ulong FileF = 0x2020202020202020ul;
-    public const ulong FileG = 0x4040404040404040ul;
-    public const ulong FileH = 0x8080808080808080ul;
+    public const ulong FileA = 0x8080808080808080ul;
+    public const ulong FileB = 0x4040404040404040ul;
+    public const ulong FileC = 0x2020202020202020ul;
+    public const ulong FileD = 0x1010101010101010ul;
+    public const ulong FileE = 0x0808080808080808ul;
+    public const ulong FileF = 0x0404040404040404ul;
+    public const ulong FileG = 0x0202020202020202ul;
+    public const ulong FileH = 0x0101010101010101ul;
 
-    public const ulong Rank1 = 0xFF00000000000000ul;
-    public const ulong Rank2 = 0x00FF000000000000ul;
-    public const ulong Rank3 = 0x0000FF0000000000ul;
-    public const ulong Rank4 = 0x000000FF00000000ul;
-    public const ulong Rank5 = 0x00000000FF000000ul;
-    public const ulong Rank6 = 0x0000000000FF0000ul;
-    public const ulong Rank7 = 0x000000000000FF00ul;
-    public const ulong Rank8 = 0x00000000000000FFul;
+    public const ulong Rank1 = 0x00000000000000FFul;
+    public const ulong Rank2 = 0x000000000000FF00ul;
+    public const ulong Rank3 = 0x0000000000FF0000ul;
+    public const ulong Rank4 = 0x00000000FF000000ul;
+    public const ulong Rank5 = 0x000000FF00000000ul;
+    public const ulong Rank6 = 0x0000FF0000000000ul;
+    public const ulong Rank7 = 0x00FF000000000000ul;
+    public const ulong Rank8 = 0xFF00000000000000ul;
 
     /// <summary>
     /// Represents the files of the chess board.
@@ -56,10 +57,43 @@ public class Board
     };
 
     /// <summary>
+    /// Represents the bitboards for the black pieces.
+    /// </summary>
+    public Dictionary<string, ulong> BlackPieces { get; } = new()
+    {
+        { "b", 0ul },
+        { "n", 0ul },
+        { "r", 0ul },
+        { "q", 0ul },
+        { "k", 0ul },
+        { "p", 0ul },
+    };
+
+    public ulong BlackPiecesBitBoard => this.BlackPieces.Values.Aggregate((a, b) => a | b);
+
+    public ulong OccupiedBitBoard => this.BlackPiecesBitBoard | this.WhitePiecesBitBoard;
+
+    /// <summary>
+    /// Represents the bitboards for the white pieces.
+    /// </summary>
+    public Dictionary<string, ulong> WhitePieces { get; } = new()
+    {
+        { "B", 0ul },
+        { "N", 0ul },
+        { "R", 0ul },
+        { "Q", 0ul },
+        { "K", 0ul },
+        { "P", 0ul },
+    };
+
+    public ulong WhitePiecesBitBoard => this.WhitePieces.Values.Aggregate((a, b) => a | b);
+
+    /// <summary>
     /// This method returns the bitboard for a given square represented by
     /// its standard chess notation.
     /// </summary>
-    public ulong Square(string? square)
+    /// <param name="square">The square in standard chess notation.</param>
+    public ulong GetSquareBitBoard(string? square)
     {
         if (square == null)  { throw new ArgumentNullException(nameof(square)); }
 
@@ -68,7 +102,7 @@ public class Board
             throw new ArgumentException("Square notation must be 2 characters long.", nameof(square));
         }
 
-        var file = square[0] - 'a';
+        int file = char.ToLower(square[0]) - 'a';
         if (file is < 0 or > 7)
         {
             throw new ArgumentException("Invalid file.", nameof(square));
@@ -81,5 +115,28 @@ public class Board
         }
 
         return this.files[file] & this.ranks[rank];
+    }
+
+    public void InitializeGameStartingBoard()
+    {
+        // Black pieces
+        this.BlackPieces["r"] = this.GetSquareBitBoard("a8") | this.GetSquareBitBoard("h8");
+        this.BlackPieces["n"] = this.GetSquareBitBoard("b8") | this.GetSquareBitBoard("g8");
+        this.BlackPieces["b"] = this.GetSquareBitBoard("c8") | this.GetSquareBitBoard("f8");
+        this.BlackPieces["q"] = this.GetSquareBitBoard("d8");
+        this.BlackPieces["k"] = this.GetSquareBitBoard("e8");
+        this.BlackPieces["p"] = this.GetSquareBitBoard("a7") | this.GetSquareBitBoard("b7") |
+            this.GetSquareBitBoard("c7") | this.GetSquareBitBoard("d7") | this.GetSquareBitBoard("e7") |
+            this.GetSquareBitBoard("f7") | this.GetSquareBitBoard("g7") | this.GetSquareBitBoard("h7");
+
+        // White pieces
+        this.WhitePieces["R"] = this.GetSquareBitBoard("a1") | this.GetSquareBitBoard("h1");
+        this.WhitePieces["N"] = this.GetSquareBitBoard("b1") | this.GetSquareBitBoard("g1");
+        this.WhitePieces["B"] = this.GetSquareBitBoard("c1") | this.GetSquareBitBoard("f1");
+        this.WhitePieces["Q"] = this.GetSquareBitBoard("d1");
+        this.WhitePieces["K"] = this.GetSquareBitBoard("e1");
+        this.WhitePieces["P"] = this.GetSquareBitBoard("a2") | this.GetSquareBitBoard("b2") |
+            this.GetSquareBitBoard("c2") | this.GetSquareBitBoard("d2") | this.GetSquareBitBoard("e2") |
+            this.GetSquareBitBoard("f2") | this.GetSquareBitBoard("g2") | this.GetSquareBitBoard("h2");
     }
 }
--- tests/Minerva.Tests/BoardTests.cs
+++ tests/Minerva.Tests/BoardTests.cs
@@ -4,31 +4,29 @@ namespace Minerva.Tests;
 public class BoardTests
 {
     [TestMethod]
-    [DataRow("a1", Board.Rank1 & Board.FileA, DisplayName = "A1")]
-    [DataRow("a8", Board.Rank8 & Board.FileA, DisplayName = "A8")]
-    [DataRow("h1", Board.Rank1 & Board.FileH, DisplayName = "H1")]
-    [DataRow("h8", Board.Rank8 & Board.FileH, DisplayName = "H8")]
+    [DataRow("a1", Board.Rank1 & Board.FileA, DisplayName = "a1")]
+    [DataRow("a8", Board.Rank8 & Board.FileA, DisplayName = "a8")]
+    [DataRow("h1", Board.Rank1 & Board.FileH, DisplayName = "h1")]
+    [DataRow("h8", Board.Rank8 & Board.FileH, DisplayName = "h8")]
+    [DataRow("A1", Board.Rank1 & Board.FileA, DisplayName = "A1")]
+    [DataRow("A8", Board.Rank8 & Board.FileA, DisplayName = "A8")]
+    [DataRow("H1", Board.Rank1 & Board.FileH, DisplayName = "H1")]
+    [DataRow("H8", Board.Rank8 & Board.FileH, DisplayName = "H8")]
     public void GetSquareReturnsTheRightBitboard(string square, ulong result)
     {
         var board = new Board();
-        var bitboard = board.Square(square);
+        ulong bitboard = board.GetSquareBitBoard(square);
         Assert.AreEqual(result, bitboard);
     }
 
     [TestMethod]
-    public void SquareH1IsRepresentedByTheMostSignicantBit()
+    [DataRow("a8", 1ul << 63, DisplayName = "a8 - MSB")]
+    [DataRow("H1", 1ul, DisplayName = "H1 - LSB")]
+    public void MostSignificanAndLeastSignificantBitSqaresAreSetProperly(string square, ulong expectedBitBoard)
     {
         var board = new Board();
-        var bitboard = board.Square("h1");
-        Assert.AreEqual(0x8000000000000000ul, bitboard);
-    }
-
-    [TestMethod]
-    public void SquareA8IsRepresentedByTheLeastSignicantBit()
-    {
-        var board = new Board();
-        var bitboard = board.Square("a8");
-        Assert.AreEqual(0x0000000000000001ul, bitboard);
+        ulong bitboard = board.GetSquareBitBoard(square);
+        Assert.AreEqual(expectedBitBoard, bitboard);
     }
 
     [TestMethod]
@@ -50,8 +48,8 @@ public class BoardTests
     public void SquareThrowsTheRightArgumentExceptions(string? square, Type exceptionType, string message)
     {
         var board = new Board();
-        var exception = Assert.ThrowsException<ArgumentException>(
-            () => board.Square(square),
+        ArgumentException exception = Assert.ThrowsException<ArgumentException>(
+            () => board.GetSquareBitBoard(square),
             "Wrong exception type.");
         Assert.AreEqual(exceptionType, exception.GetType());
         Assert.AreEqual(message, exception.Message);
@@ -61,10 +59,98 @@ public class BoardTests
     public void SquareThrowsArgumentNullExceptionWhenSquareIsNull()
     {
         var board = new Board();
-        var exception = Assert.ThrowsException<ArgumentNullException>(
-            () => board.Square(null),
+        ArgumentNullException exception = Assert.ThrowsException<ArgumentNullException>(
+            () => board.GetSquareBitBoard(null),
             "Wrong exception type.");
         Assert.AreEqual("square", exception.ParamName);
         Assert.AreEqual("Value cannot be null. (Parameter 'square')", exception.Message);
+    }
+
+    [TestMethod]
+    public void BlackPiecesBitBoardIsCorrectOnEmptyBoardCreation()
+    {
+        var board = new Board();
+        ulong blackPiecesBitBoard = board.BlackPieces.Values.Aggregate((a, b) => a | b);
+        Assert.AreEqual(0ul, blackPiecesBitBoard);
+    }
+
+    [TestMethod]
+    public void WhitePiecesBitBoardIsCorrectOnEmptyBoardCreation()
+    {
+        var board = new Board();
+        ulong whitePiecesBitBoard = board.WhitePieces.Values.Aggregate((a, b) => a | b);
+        Assert.AreEqual(0ul, whitePiecesBitBoard);
+    }
+
+    [TestMethod]
+    public void OccupiedBitBoardIsCorrectOnEmptyBoardCreation()
+    {
+        var board = new Board();
+        ulong occupiedBitBoard = board.OccupiedBitBoard;
+        Assert.AreEqual(0ul, occupiedBitBoard);
+    }
+
+    [TestMethod]
+    public void BlackPiecesBitBoardIsCorrectAfterSettingBlackPieces()
+    {
+        var board = new Board { BlackPieces = { ["p"] = 1ul } };
+        ulong blackPiecesBitBoard = board.BlackPieces.Values.Aggregate((a, b) => a | b);
+        Assert.AreEqual(1ul, blackPiecesBitBoard);
+    }
+
+    [TestMethod]
+    public void AllBitBoardsAreCorrectAfterCreatingGameStartingBoard()
+    {
+        var board = new Board();
+        board.InitializeGameStartingBoard();
+
+        // Bitboards
+        Assert.AreEqual(0x000000000000FFFFul, board.WhitePiecesBitBoard, "White Pieces.");
+        Assert.AreEqual(0xFFFF000000000000ul, board.BlackPiecesBitBoard, "Black pieces.");
+        Assert.AreEqual(0xFFFF00000000FFFFul, board.OccupiedBitBoard, "Occupied squares.");
+        
+        // Black pieces
+        Assert.AreEqual(
+            board.GetSquareBitBoard("a8") | board.GetSquareBitBoard("h8"),
+            board.BlackPieces["r"],
+            "Black rocks");
+        Assert.AreEqual(
+            board.GetSquareBitBoard("b8") | board.GetSquareBitBoard("g8"),
+            board.BlackPieces["n"],
+            "Black knights");
+        Assert.AreEqual(
+            board.GetSquareBitBoard("c8") | board.GetSquareBitBoard("f8"),
+            board.BlackPieces["b"],
+            "Black bishops");
+        Assert.AreEqual(board.GetSquareBitBoard("d8"), board.BlackPieces["q"], "Black queen");
+        Assert.AreEqual(board.GetSquareBitBoard("e8"), board.BlackPieces["k"], "Black king");
+        Assert.AreEqual(
+            board.GetSquareBitBoard("a7") | board.GetSquareBitBoard("b7") | board.GetSquareBitBoard("c7") |
+            board.GetSquareBitBoard("d7") | board.GetSquareBitBoard("e7") | board.GetSquareBitBoard("f7") |
+            board.GetSquareBitBoard("g7") | board.GetSquareBitBoard("h7"),
+            board.BlackPieces["p"],
+            "Black pawns");
+
+        // White pieces
+        Assert.AreEqual(
+            board.GetSquareBitBoard("a1") | board.GetSquareBitBoard("h1"),
+            board.WhitePieces["R"],
+            "White rocks");
+        Assert.AreEqual(
+            board.GetSquareBitBoard("b1") | board.GetSquareBitBoard("g1"),
+            board.WhitePieces["N"],
+            "White knights");
+        Assert.AreEqual(
+            board.GetSquareBitBoard("c1") | board.GetSquareBitBoard("f1"),
+            board.WhitePieces["B"],
+            "White bishops");
+        Assert.AreEqual(board.GetSquareBitBoard("d1"), board.WhitePieces["Q"], "White queen");
+        Assert.AreEqual(board.GetSquareBitBoard("e1"), board.WhitePieces["K"], "White king");
+        Assert.AreEqual(
+            board.GetSquareBitBoard("a2") | board.GetSquareBitBoard("b2") | board.GetSquareBitBoard("c2") |
+            board.GetSquareBitBoard("d2") | board.GetSquareBitBoard("e2") | board.GetSquareBitBoard("f2") |
+            board.GetSquareBitBoard("g2") | board.GetSquareBitBoard("h2"),
+            board.WhitePieces["P"],
+            "White pawns");
     }
 }
