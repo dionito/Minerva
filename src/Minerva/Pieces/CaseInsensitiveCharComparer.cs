090c39e0512cb7367d8a39d2aca0b43aed84c58a
dionito <12321827+dionito@users.noreply.github.com>
2024-07-03 20:08:12 -0700

    Adding Test UI, missing files #41

---
+++ src/Minerva/Pieces/CaseInsensitiveCharComparer.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (C) 2024 dionito
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+namespace Minerva.Pieces;
+
+public class CaseInsensitiveCharComparer : IEqualityComparer<char>
+{
+    public bool Equals(char x, char y)
+    {
+        return char.ToUpperInvariant(x) == char.ToUpperInvariant(y);
+    }
+
+    public int GetHashCode(char obj)
+    {
+        return char.ToUpperInvariant(obj).GetHashCode();
+    }
+}
